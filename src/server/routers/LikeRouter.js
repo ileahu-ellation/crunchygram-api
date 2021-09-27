bf4e4bc386a018f0d95cd48115f21b71c992be91
Igor Leahu <56587882+ileahu-ellation@users.noreply.github.com>
2021-09-27 19:19:24 +0300

    Add /like/me

--- src/server/routers/LikeRouter.js
+++ src/server/routers/LikeRouter.js
@@ -1,8 +1,8 @@
-import { propEq } from 'ramda';
+import { compose, flip, includes, prop, propEq } from 'ramda';
 import Router from '../util/Router.js';
 import Like from '../../db/entities/Like.js';
 import Post from '../../db/entities/Post.js';
-import { POST } from '../util/constants.js';
+import { GET, POST } from '../util/constants.js';
 import { paramsValidatorMiddleware } from '../middlewares/validatorMiddleware.js';
 import requireAuthMiddleware from '../middlewares/authenticationMiddleware.js';
 
@@ -28,6 +28,8 @@ class LikeRouter extends Router {
       }),
       this.toggle,
     );
+
+    this.addRoute(GET, '/me', requireAuthMiddleware(), this.list);
   }
 
   async toggle(req, res) {
@@ -43,6 +45,17 @@ class LikeRouter extends Router {
     }
 
     res.status(201).send();
+  }
+
+  async list(req, res) {
+    const { username } = req.session;
+
+    const likedPostIds = Like.list(propEq('username', username)).map(
+      prop('postId'),
+    );
+    const posts = Post.list(({ id }) => likedPostIds.includes(id));
+
+    res.send(posts);
   }
 }
 
