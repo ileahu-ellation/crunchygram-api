59c03e14cbdf37085ebaa476be92ed5f17599a0e
Igor Leahu <56587882+ileahu-ellation@users.noreply.github.com>
2021-09-28 11:15:55 +0300

    Generate post ids on defaultDb

--- src/db/util/dbDefaults.js
+++ src/db/util/dbDefaults.js
@@ -1,9 +1,14 @@
+import randomID from './randomID.js';
+
 const getDefaultDbData = async () => {
   const users = await import('../../constants/users.js').then(
     data => data.default,
   );
-  const posts = await import('../../constants/posts.js').then(
-    data => data.default,
+  const posts = await import('../../constants/posts.js').then(data =>
+    data.default.map(item => ({
+      id: randomID(),
+      ...item,
+    })),
   );
 
   return {
